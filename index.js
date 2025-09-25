8b902bea7946b61053bdd08957b607eb5085d4ab
joshwizard <murikij@gmail.com>
2025-09-25 16:20:10 +0300

    Added DOM code

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+const main = document.getElementById("main")
+const removeMain = document.querySelector("main")
+main.remove(removeMain)
+
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.textContent = 'Joshua is the champion'
+document.body.append(newHeader)
+
